97f811ae69c368183b1f22f77270827c90d1bd27
zgorewski <bartekzgorewski@gmail.com>
2023-11-22 14:38:15 +0100

    Added working .h file

---
+++ funkcjeDef.h
@@ -0,0 +1,12 @@
+#ifndef B_DRZEWO_FUNKCJEDEF_H
+#define B_DRZEWO_FUNKCJEDEF_H
+
+#include <iostream>
+#include "odczyt.cpp"
+
+using namespace std;
+
+// Calling the functions here...
+void Odczyt(char fileName[]);
+
+#endif //B_DRZEWO_FUNKCJEDEF_H
--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
-#include "odczyt.cpp"
+#include "funkcjeDef.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    for(int i=1; i<argc; i++)
-        Odczyt(argv[i]);
+    if(argc > 1) {
+        for (int i = 1; i < argc; i++)
+            Odczyt(argv[i]);
+    }
+    else{
+        cout << "Aby otworzyc plik, wywolaj funkcje jeszcze raz, i podaj jego poprawna nazwe... (call the function with ./main *filename.txt*)" << endl;
+    }
 }
